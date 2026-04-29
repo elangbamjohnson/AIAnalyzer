0e18dc573dc0e4bfe1e7c6b7d0ae7dcf5c68465e
Johnson <elangbamj@avaya.com>
2026-04-29 18:00:28 +0530

    Add comprehensive rule stress test scenario
    - Add test files to trigger LargeClass, HighMethodDensity, GodObject, and DataHeavyClass rules
    - Verify architectural-aware thresholds and multi-signal detection logic

---
+++ TestSandbox/RulesStressTest/LargeService.swift
@@ -0,0 +1,6 @@
+class AnalyticsService {
+    func m1() {}; func m2() {}; func m3() {}; func m4() {}; func m5() {}
+    func m6() {}; func m7() {}; func m8() {}; func m9() {}; func m10() {}
+    func m11() {}; func m12() {}; func m13() {}; func m14() {}; func m15() {}
+    func m16() {} 
+}
---
+++ TestSandbox/RulesStressTest/UltimateGodViewController.swift
@@ -0,0 +1,14 @@
+class MainAppViewController: UIViewController {
+    var p1=1, p2=1, p3=1, p4=1, p5=1, p6=1, p7=1, p8=1, p9=1, p10=1
+    var p11=1, p12=1, p13=1, p14=1, p15=1, p16=1, p17=1, p18=1, p19=1, p20=1, p21=1
+    
+    func m1(){}; func m2(){}; func m3(){}; func m4(){}; func m5(){}
+    func m6(){}; func m7(){}; func m8(){}; func m9(){}; func m10(){}
+    func m11(){}; func m12(){}; func m13(){}; func m14(){}; func m15(){}
+    func m16(){}; func m17(){}; func m18(){}; func m19(){}; func m20(){}
+    func m21(){}; func m22(){}; func m23(){}; func m24(){}; func m25(){}
+    func m26(){}; func m27(){}; func m28(){}; func m29(){}; func m30(){}
+    func m31(){}; func m32(){}; func m33(){}; func m34(){}; func m35(){}
+    func m36(){}; func m37(){}; func m38(){}; func m39(){}; func m40(){}
+    func m41(){}
+}
